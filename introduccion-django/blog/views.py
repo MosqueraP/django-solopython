4ab2ff6c898313ecd60e2080f4012f04711f5212
Plinio <mcplinio.15@gmail.com>
2024-01-18 01:09:09 -0500

    configuracion de de formulario y vistas post y envio de informacion a la bd

--- introduccion-django/blog/views.py
+++ introduccion-django/blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from blog.forms import PosteCreateForm
+from blog.models import Post
 
 # Create your views here.
 
@@ -11,12 +12,25 @@ class BlogListView(View):
         return render(request, 'blog_list.html', context)
 
 class BlogCreateView(View):
+    # obtener informacion
     def get(self, request, *args, **kw):
+        form = PosteCreateForm
         context={
+            'form': form
         }
         return render(request, 'blog_create.html', context)
     
+    # enviar informacion
     def post(self, request, *args, **kw):
+        if request.method == 'POST':
+            form = PosteCreateForm(request.POST)
+            if form.is_valid():
+                _title = form.cleaned_data.get('title')
+                _content = form.cleaned_data.get('content')
+                p, created = Post.objects.get_or_create(title=_title, content=_content)
+                p.save()
+                return redirect('blog:home')
+            
         context={
         }
         return render(request, 'blog_create.html', context)
